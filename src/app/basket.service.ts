8b5de6725aa144f14aa1f1b1d0915f82cca72613
Charlotte-Overeem <r0781396@student.thomasmore.be>
2021-12-15 17:25:51 +0100

    resolved a second major merge conflict

--- src/app/basket.service.ts
+++ src/app/basket.service.ts
@@ -15,27 +15,4 @@ export class BasketService {
     return this.httpClient.get<Basket[]>("http://localhost:3000/baskets?userId=" + userId.toString())
   }
 
-  getBaskets(): Observable<Basket[]> {
-    return this.httpClient.get<Basket[]>(this.url + "/baskets");
-  }
-
-  getBasketById(id: string): Observable<Basket> {
-    return this.httpClient.get<Basket>(this.url + "/baskets/" + id);
-  }
-
-  postBasket(basket: Basket):  Observable<Basket>{
-    let headers = new HttpHeaders();
-    headers = headers.set('Content-Type', 'application/json; charset=utf-8');
-    return this.httpClient.post<Basket>(this.url + "/baskets", basket, {headers: headers});
-  }
-
-  putBasket(id: string, basket: Basket): Observable<Basket>{
-    let headers = new HttpHeaders();
-    headers = headers.set('Content-Type', 'application/json; charset=utf-8');
-    return this.httpClient.put<Basket>(this.url + "/baskets/"+id , basket, {headers: headers});
-  }
-
-  deleteBasket(id: string): Observable<Basket> {
-    return this.httpClient.delete<Basket>(this.url + "/baskets/" + id);
-  }
 }
