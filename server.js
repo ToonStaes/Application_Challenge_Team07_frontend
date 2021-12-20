13c7c34347015cf947844ed2e71d6eebbd7404ba
Toon Staes <r0784094@student.thomasmore.be>
2021-12-20 20:20:46 +0100

    another typo

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const path = require("path");
 const app = express();
 app.use(express.static(__dirname + "/dist/angular-project"));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/angular-project/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/Angular-project/index.html"));
 });
 app.listen(process.env.PORT || 8080);
